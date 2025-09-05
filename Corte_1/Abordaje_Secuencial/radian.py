fdba05648a4d1a79aa2a1691ab1cc32cf727f89e
Karoll-v <karoll.vargas.1101@gmail.com>
2025-09-04 21:06:07 -0500

    Update radian.py

--- Corte_1/Abordaje_Secuencial/radian.py
+++ Corte_1/Abordaje_Secuencial/radian.py
@@ -1,11 +1,12 @@
 import math
 
 degrees=45
-radianes=degrees/180.0*math.pi
+radianes=math.radians(degrees)
 math.sin(radianes)
 seno=math.sin(degrees)
 senr=math.sin(radianes)
 
 print(radianes)
 print(seno)
+
 print(senr)
