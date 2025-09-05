2e66368afe316b4118a0fee1e8cf5e9e173021e5
Karoll-v <karoll.vargas.1101@gmail.com>
2025-09-04 21:17:36 -0500

    Add files via upload

---
+++ Corte_1/Abordaje_Secuencial/rad.py
@@ -0,0 +1,11 @@
+import math
+
+degrees = 45
+radianes = math.radians(degrees)  
+
+seno = math.sin(radianes)  
+senr = math.sin(radianes)  
+
+print("Radianes:", radianes)
+print("Seno (de 45°):", seno)
+print("Seno (radianes):", senr)
